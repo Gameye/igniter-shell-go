e4b4dc264e1f39bd93b415223e3278cb306ab440
Elmer Bulthuis <elmerbulthuis@gmail.com>
2019-02-26 17:28:28 +0100

    tf2 conf

--- command/launch.go
+++ command/launch.go
@@ -1,15 +1,17 @@
 package command
 
 import (
+	"encoding/json"
 	"os"
 	"os/exec"
 
 	"github.com/elmerbulthuis/shell-go/shell"
-	"github.com/elmerbulthuis/shell-go/utils"
+	"github.com/elmerbulthuis/shell-go/statemachine"
 	"github.com/spf13/cobra"
 )
 
 var emulateTTY bool
+var configFile string
 
 // LaunchCommand launches a process
 var LaunchCommand = &cobra.Command{
@@ -30,18 +32,60 @@ func init() {
 			false,
 			"Emulate a TTY for the child process",
 		)
+
+	LaunchCommand.
+		PersistentFlags().
+		StringVarP(
+			&configFile,
+			"config-file",
+			"c",
+			"",
+			"Path to config file",
+		)
+
 }
 
 func runLaunchCommand(cmd *cobra.Command, args []string) (err error) {
-	proc := exec.Command(args[0], args[1:]...)
-	config := utils.MakeTf2Config()
+	config, err := loadConfig(configFile)
+	if err != nil {
+		return
+	}
 
-	exit, err := shell.RunWithStateMachine(proc, config, emulateTTY)
+	proc := exec.Command(args[0], args[1:]...)
+
+	exit, err := shell.RunWithStateMachine(
+		proc,
+		config,
+		emulateTTY,
+	)
 	if err != nil {
 		return
 	}
 
 	os.Exit(exit)
+
+	return
+}
+
+func loadConfig(
+	configFile string,
+) (
+	config *statemachine.Config,
+	err error,
+) {
+	config = &statemachine.Config{}
+
+	file, err := os.OpenFile(configFile, os.O_RDONLY, 0)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(config)
+	if err != nil {
+		return
+	}
 
 	return
 }
